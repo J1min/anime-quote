67c03ff8f52b7b6dcc5dece770c6c297d87f2061
J1min <aoiharu41@gmail.com>
2023-03-14 10:40:39 +0900

    Feat: 실습문제 추가

--- node/index.js
+++ node/index.js
@@ -1,8 +1,66 @@
-// http모듈을 추출합니다.
 const http = require("http");
+const url = require("url");
+const fs = require("fs");
+const path = require("path");
 
-const server = http.createServer();
+const port = 8080;
 
-server.listen(3001, function () {
-  console.log("3001번 포트로 서버가 실행되었습니다.");
+const server = http.createServer((req, res) => {
+  const { pathname, query } = url.parse(req.url, true);
+
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/plain");
+
+  switch (pathname) {
+    case "/":
+      fs.readFile(path.join(__dirname, "index.html"), (err, data) => {
+        if (err) {
+          res.statusCode = 500;
+          res.end();
+          return;
+        }
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        res.end(data);
+      });
+      break;
+
+    case "/admin":
+      fs.readFile(path.join(__dirname, "admin.js"), (err, data) => {
+        if (err) {
+          res.statusCode = 500;
+          res.end();
+          return;
+        }
+        res.statusCode = 200;
+        res.setHeader("Content-Type", "text/html");
+        res.end(data);
+      });
+      break;
+
+    case "/hello":
+      res.end(`Hello, ${query.name}!`);
+      break;
+
+    default:
+      res.statusCode = 404;
+      res.end();
+      break;
+  }
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://localhost:${port}/`);
+});
+
+fs.readFile(filePath, (err, data) => {
+  if (err) {
+    res.statusCode = 500;
+    res.end();
+    return;
+  }
+
+  res.statusCode = 200;
+  res.setHeader("Content-Type", "text/html");
+  res.end(data);
 });
