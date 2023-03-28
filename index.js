41de6d2b7f7afcda4d3d407ea3ed51f9e1948070
J1min <aoiharu41@gmail.com>
2023-03-28 11:54:00 +0900

    로직 추가

--- index.js
+++ index.js
@@ -3,33 +3,36 @@ const mysql = require("mysql2");
 
 const app = express();
 
-const connection = mysql.createConnection({
-  host: "127.0.0.1",
-  user: "root",
-  password: "1234",
-  port: "3306",
-  database: "my_db",
-});
-
-connection.connect((err) => {
-  if (err) {
-    console.error("Error connecting to MySQL database");
-    return;
-  }
-  console.log("Connected to MySQL database");
-});
-
 app.get("/", (req, res) => {
   res.send("안녕하세용");
 });
 
 app.get("/user/:id", (req, res) => {
   console.log();
+  const connection = mysql.createConnection({
+    host: "127.0.0.1",
+    user: "root",
+    password: "1234",
+    port: "3306",
+    database: "my_db",
+  });
+
+  connection.connect((err) => {
+    if (err) {
+      console.error("Error connecting to MySQL database");
+      return;
+    }
+    console.log("Connected to MySQL database");
+  });
+
   const sql = `select name from NewTable where pw='${req.params.id}'`;
+
   connection.query(sql, (_, results) => {
     res.send(results[0].name);
     console.log(results);
   });
+
+  connection.end();
 });
 
 app.listen(8080, () => {
