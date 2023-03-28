00e7798ef085fa21c79802d8a51f733f5923e893
J1min <aoiharu41@gmail.com>
2023-03-28 11:16:28 +0900

    웹서비스 추가

--- index.js
+++ index.js
@@ -4,20 +4,30 @@ const mysql = require("mysql2");
 const app = express();
 
 const connection = mysql.createConnection({
-  host: "localhost",
+  host: "127.0.0.1",
   user: "root",
   password: "1234",
   port: "3306",
   database: "my_db",
 });
 
-app.get("/", () => {
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL database");
+    return;
+  }
+  console.log("Connected to MySQL database");
+});
+
+app.get("/", (req, res) => {
   res.send("안녕하세용");
 });
 
-app.get("/user", (req, res) => {
-  const sql = "select * from NewTable";
-  connection.query(sql, (err, results) => {
+app.get("/user/:id", (req, res) => {
+  console.log();
+  const sql = `select name from NewTable where pw='${req.params.id}'`;
+  connection.query(sql, (_, results) => {
+    res.send(results[0].name);
     console.log(results);
   });
 });
