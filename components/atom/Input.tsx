99ff645a0a33dba5eb2981566d2f8c12e2896929
J1min <aoiharu41@gmail.com>
2023-04-05 21:45:02 +0900

    feat: input atoms 추가

---
+++ components/atom/Input.tsx
@@ -0,0 +1,31 @@
+import theme from "@/styles/theme";
+import { InputHTMLAttributes } from "react";
+import { UseFormRegisterReturn } from "react-hook-form";
+import styled from "styled-components";
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  registerReturn?: UseFormRegisterReturn;
+}
+
+export default function Input({
+  type = "text",
+  registerReturn,
+  ...props
+}: InputProps) {
+  return <InputView type={type} {...registerReturn} {...props} />;
+}
+
+const InputView = styled.input<InputProps>`
+  background-color: ${theme.primary}
+  color:  ${theme.primary}
+  width: 75%;
+  max-width: 40rem;
+  border: 0;
+  box-sizing: border-box;
+  border-radius: 0.375rem;
+  padding: 0.5rem 1rem;
+  filter: drop-shadow(0px 2px 2px rgba(0, 0, 0, 0.25));
+  &:focus {
+    outline: none;
+  }
+`;
