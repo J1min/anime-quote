4cebe778da772664ba3fc5906f4178439bff449b
J1min <aoiharu41@gmail.com>
2023-04-05 21:35:50 +0900

    feat: QuoteListView 추가

---
+++ components/atom/QuoteListView.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+import { Quote } from "@/types/quote.interface";
+import QuoteView from "./QuoteView";
+
+interface QuoteViewProps {
+  quoteList: Quote[];
+}
+
+export default function QuoteListView({ quoteList }: QuoteViewProps) {
+  return (
+    <QuoteListWrapper>
+      {quoteList.map((quote) => (
+        <QuoteView key={quote.quote_id} quote={quote} />
+      ))}
+    </QuoteListWrapper>
+  );
+}
+
+const QuoteListWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.5rem;
+`;
