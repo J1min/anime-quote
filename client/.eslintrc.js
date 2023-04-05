d3c4e8504f9c238a1bdd08c2344ffd3843730cf6
J1min <aoiharu41@gmail.com>
2023-04-05 10:29:09 +0900

    Fix: 파일 경로 변경

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -3,7 +3,7 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint", "prettier"],
   parserOptions: {
-    project: "./tsconfig.json",
+    project: "/Users/J1min/Desktop/Code/Anime-Quote/client/tsconfig.json",
   },
   env: {
     node: true,
