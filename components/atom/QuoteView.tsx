2a87383910c743eb23baf01ba37a086d73e25a36
J1min <aoiharu41@gmail.com>
2023-04-05 20:41:02 +0900

    feat: 명언 조회 atoms 추가

---
+++ components/atom/QuoteView.tsx
@@ -0,0 +1,46 @@
+import styled from "styled-components";
+import { Quote } from "@/types/quote.interface";
+import theme from "@/styles/theme";
+import QuoteIcon from "../icons/QuoteIcon";
+
+interface QuoteViewProps {
+  quote: Quote;
+}
+
+export default function QuoteView({ quote }: QuoteViewProps) {
+  return (
+    <QuoteWrapper>
+      <QuoteIcon />
+      <QuoteContent>{quote.quote_content}</QuoteContent>
+      <QuoteCharactorName>-{quote.charactor_name}-</QuoteCharactorName>
+    </QuoteWrapper>
+  );
+}
+
+const QuoteWrapper = styled.article`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+  gap: 1.5rem;
+
+  max-width: 45rem;
+  width: 75%;
+
+  word-break: keep-all;
+  line-height: 3.5rem;
+
+  border: 1px solid ${theme.primary};
+  padding: 3.75rem 7.5rem;
+  box-sizing: border-box;
+`;
+
+const QuoteContent = styled.span`
+  font-size: 2.5rem;
+`;
+
+const QuoteCharactorName = styled.span`
+  font-family: "Chosunilbo_myungjo";
+  font-size: 1.5rem;
+  letter-spacing: 0.2rem;
+`;
