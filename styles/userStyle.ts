17dcb3b7f5f26d355b782a2ea8fdf1206da3cd32
J1min <aoiharu41@gmail.com>
2023-04-05 20:41:49 +0900

    feat: 조선일보-명조체 추가

--- styles/userStyle.ts
+++ styles/userStyle.ts
@@ -4,8 +4,15 @@ const UserStyle = createGlobalStyle`
 @font-face {
   font-family: 'Pretendard-Regular';
   src: url('https://cdn.jsdelivr.net/gh/Project-Noonnu/noonfonts_2107@1.1/Pretendard-Regular.woff') format('woff');
-  font-weight: 400;
+  font-weight: 200;
   font-style: normal;
+}
+
+@font-face {
+    font-family: 'Chosunilbo_myungjo';
+    src: url('https://cdn.jsdelivr.net/gh/projectnoonnu/noonfonts_one@1.0/Chosunilbo_myungjo.woff') format('woff');
+    font-weight: normal;
+    font-style: normal;
 }
 
 html {
