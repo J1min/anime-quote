650cac619956f44a72d088904582eb87cf28c59f
J1min <aoiharu41@gmail.com>
2023-04-05 09:39:30 +0900

    Feat: DB url 환경변수로 분리

--- database.py
+++ database.py
@@ -1,11 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
 
 import json
 import os
 
+load_dotenv()
 
-
-DATABASE_URL = 'mysql://root:12345@localhost:3306/anime_script'
+DATABASE_URL = os.environ.get('QUOTE_DATABASE_URL')
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
